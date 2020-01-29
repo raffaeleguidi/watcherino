202ddd02ab95e249fc92cabfee7d40bd03bff23f
Raffaele P. Guidi <raffaele.p.guidi@gmail.com>
2020-01-29 10:31:04 +0100

    fixed missing variable

--- test/example.sh
+++ test/example.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # the script receives three arguments: folder, file and CREATE|WRITEPARAM_FOLDER=$1
+PARAM_FOLDER=$1
 PARAM_FILE=$2
 PARAM_EVENT=$3
 echo "hi, this is watcherino on folder '${PARAM_FOLDER}' file '${PARAM_FILE}' with event '${PARAM_EVENT}'" >> test.log
